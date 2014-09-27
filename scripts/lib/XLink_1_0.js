de79f06d0c4a12a2d4f9b3557d1b3067c65517a9
highsource <aleksei.valikov@gmail.com>
2014-09-27 19:16:40 +0200

    Using the new Jsonix compiler version.

--- scripts/lib/XLink_1_0.js
+++ scripts/lib/XLink_1_0.js
@@ -4,12 +4,109 @@ var XLink_1_0_Module_Factory = function () {
     dens: 'http:\/\/www.w3.org\/1999\/xlink',
     dans: 'http:\/\/www.w3.org\/1999\/xlink',
     tis: [{
+        ln: 'Simple',
+        ps: [{
+            n: 'content',
+            col: true,
+            t: 'ae'
+          }, {
+            n: 'type',
+            t: 'a'
+          }, {
+            n: 'href',
+            t: 'a'
+          }, {
+            n: 'role',
+            t: 'a'
+          }, {
+            n: 'arcrole',
+            t: 'a'
+          }, {
+            n: 'title',
+            t: 'a'
+          }, {
+            n: 'show',
+            t: 'a'
+          }, {
+            n: 'actuate',
+            t: 'a'
+          }]
+      }, {
+        ln: 'Extended',
+        ps: [{
+            n: 'extendedModel',
+            col: true,
+            etis: [{
+                en: 'title',
+                ti: '.TitleEltType'
+              }, {
+                en: 'resource',
+                ti: '.ResourceType'
+              }, {
+                en: 'locator',
+                ti: '.LocatorType'
+              }, {
+                en: 'arc',
+                ti: '.ArcType'
+              }],
+            t: 'es'
+          }, {
+            n: 'type',
+            t: 'a'
+          }, {
+            n: 'role',
+            t: 'a'
+          }, {
+            n: 'title',
+            t: 'a'
+          }]
+      }, {
+        ln: 'LocatorType',
+        ps: [{
+            n: 'locatorTitle',
+            col: true,
+            en: 'title',
+            ti: '.TitleEltType'
+          }, {
+            n: 'type',
+            t: 'a'
+          }, {
+            n: 'href',
+            t: 'a'
+          }, {
+            n: 'role',
+            t: 'a'
+          }, {
+            n: 'title',
+            t: 'a'
+          }, {
+            n: 'label',
+            t: 'a'
+          }]
+      }, {
+        ln: 'TitleEltType',
+        ps: [{
+            n: 'content',
+            col: true,
+            t: 'ae'
+          }, {
+            n: 'type',
+            t: 'a'
+          }, {
+            n: 'lang',
+            an: {
+              lp: 'lang',
+              ns: 'http:\/\/www.w3.org\/XML\/1998\/namespace'
+            },
+            t: 'a'
+          }]
+      }, {
         ln: 'ArcType',
         ps: [{
             n: 'locatorTitle',
             col: true,
             en: 'title',
-            ti: 'XLink_1_0.TitleEltType'
+            ti: '.TitleEltType'
           }, {
             n: 'type',
             t: 'a'
@@ -52,107 +149,6 @@ var XLink_1_0_Module_Factory = function () {
             t: 'a'
           }]
       }, {
-        ln: 'TitleEltType',
-        ps: [{
-            n: 'content',
-            col: true,
-            t: 'ae'
-          }, {
-            n: 'type',
-            t: 'a'
-          }, {
-            n: 'lang',
-            an: {
-              lp: 'lang',
-              ns: 'http:\/\/www.w3.org\/XML\/1998\/namespace'
-            },
-            t: 'a'
-          }]
-      }, {
-        ln: 'LocatorType',
-        ps: [{
-            n: 'locatorTitle',
-            col: true,
-            en: 'title',
-            ti: 'XLink_1_0.TitleEltType'
-          }, {
-            n: 'type',
-            t: 'a'
-          }, {
-            n: 'href',
-            t: 'a'
-          }, {
-            n: 'role',
-            t: 'a'
-          }, {
-            n: 'title',
-            t: 'a'
-          }, {
-            n: 'label',
-            t: 'a'
-          }]
-      }, {
-        ln: 'Simple',
-        ps: [{
-            n: 'content',
-            col: true,
-            t: 'ae'
-          }, {
-            n: 'type',
-            t: 'a'
-          }, {
-            n: 'href',
-            t: 'a'
-          }, {
-            n: 'role',
-            t: 'a'
-          }, {
-            n: 'arcrole',
-            t: 'a'
-          }, {
-            n: 'title',
-            t: 'a'
-          }, {
-            n: 'show',
-            t: 'a'
-          }, {
-            n: 'actuate',
-            t: 'a'
-          }]
-      }, {
-        ln: 'Extended',
-        ps: [{
-            n: 'extendedModel',
-            col: true,
-            etis: [{
-                en: 'title',
-                ti: 'XLink_1_0.TitleEltType'
-              }, {
-                en: 'resource',
-                ti: 'XLink_1_0.ResourceType'
-              }, {
-                en: 'locator',
-                ti: 'XLink_1_0.LocatorType'
-              }, {
-                en: 'arc',
-                ti: 'XLink_1_0.ArcType'
-              }],
-            t: 'es'
-          }, {
-            n: 'type',
-            t: 'a'
-          }, {
-            n: 'role',
-            t: 'a'
-          }, {
-            n: 'title',
-            t: 'a'
-          }]
-      }, {
-        t: 'enum',
-        ln: 'TypeType',
-        vs: ['simple', 'extended', 'title', 'resource', 'locator', 'arc']
-      }, {
         t: 'enum',
         ln: 'ActuateType',
         vs: ['onLoad', 'onRequest', 'other', 'none']
@@ -160,19 +156,23 @@ var XLink_1_0_Module_Factory = function () {
         t: 'enum',
         ln: 'ShowType',
         vs: ['new', 'replace', 'embed', 'other', 'none']
+      }, {
+        t: 'enum',
+        ln: 'TypeType',
+        vs: ['simple', 'extended', 'title', 'resource', 'locator', 'arc']
       }],
     eis: [{
-        en: 'locator',
-        ti: 'XLink_1_0.LocatorType'
-      }, {
-        en: 'title',
-        ti: 'XLink_1_0.TitleEltType'
-      }, {
         en: 'resource',
-        ti: 'XLink_1_0.ResourceType'
+        ti: '.ResourceType'
+      }, {
+        en: 'locator',
+        ti: '.LocatorType'
       }, {
         en: 'arc',
-        ti: 'XLink_1_0.ArcType'
+        ti: '.ArcType'
+      }, {
+        en: 'title',
+        ti: '.TitleEltType'
       }]
   };
   return {
@@ -183,10 +183,11 @@ if (typeof define === 'function' && define.amd) {
   define([], XLink_1_0_Module_Factory);
 }
 else {
+  var XLink_1_0_Module = XLink_1_0_Module_Factory();
   if (typeof module !== 'undefined' && module.exports) {
-    module.exports.XLink_1_0 = XLink_1_0_Module_Factory().XLink_1_0;
+    module.exports.XLink_1_0 = XLink_1_0_Module.XLink_1_0;
   }
   else {
-    var XLink_1_0 = XLink_1_0_Module_Factory().XLink_1_0;
+    var XLink_1_0 = XLink_1_0_Module.XLink_1_0;
   }
 }
