64cf5717b7e3d913ee02188de63db7c0a546ac98
highsource <aleksei.valikov@gmail.com>
2015-04-18 07:13:14 +0200

    Fixed #9.

--- scripts/w3c-schemas.js
+++ scripts/w3c-schemas.js
@@ -1,12 +1,5 @@
-var schemas = [
-	"Atom_1_0",
-	"WS_Addr_1_0_Core",
-	"XHTML_1_0_Strict",
-	"XLink_1_0",
-	"XSD_1_0"
-];
-for (var index = 0; index < schemas.length; index++)
-{
-	var schema = schemas[index];
-	module.exports[schema] = require('./lib/' + schema)[schema];
-}
+module.exports.Atom_1_0         = require('./lib/Atom_1_0'        ).Atom_1_0        ;
+module.exports.WS_Addr_1_0_Core = require('./lib/WS_Addr_1_0_Core').WS_Addr_1_0_Core;
+module.exports.XHTML_1_0_Strict = require('./lib/XHTML_1_0_Strict').XHTML_1_0_Strict;
+module.exports.XLink_1_0        = require('./lib/XLink_1_0'       ).XLink_1_0       ;
+module.exports.XSD_1_0          = require('./lib/XSD_1_0'         ).XSD_1_0         ;
