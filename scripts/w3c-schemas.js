a1d5874f5090a8482c322ae0cead95ea64a7028f
highsource <aleksei.valikov@gmail.com>
2015-06-17 23:18:26 +0200

    Issue #11

--- scripts/w3c-schemas.js
+++ scripts/w3c-schemas.js
@@ -1,11 +1,32 @@
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports.Atom_1_0         = require('./lib/Atom_1_0'        ).Atom_1_0        ;
-    module.exports.WS_Addr_1_0_Core = require('./lib/WS_Addr_1_0_Core').WS_Addr_1_0_Core;
-    module.exports.XHTML_1_0_Strict = require('./lib/XHTML_1_0_Strict').XHTML_1_0_Strict;
-    module.exports.XLink_1_0        = require('./lib/XLink_1_0'       ).XLink_1_0       ;
-    module.exports.XSD_1_0          = require('./lib/XSD_1_0'         ).XSD_1_0         ;
-} else {
-    // Browser context
-    // You may include the modules by hand
-    console.warn('w3c-schemas in a browser context, you may include the modules by hand');
-}
+(function (root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(['exports',
+			'./lib/Atom_1_0',
+			'./lib/WS_Addr_1_0_Core',
+			'./lib/XHTML_1_0_Strict',
+			'./lib/XLink_1_0',
+			'./lib/XSD_1_0'
+		], factory);
+	} else if (typeof module === 'object' && typeof exports === 'object') {
+		factory(exports,
+			require('./lib/Atom_1_0'        ).Atom_1_0,
+			require('./lib/WS_Addr_1_0_Core').WS_Addr_1_0_Core,
+			require('./lib/XHTML_1_0_Strict').XHTML_1_0_Strict,
+			require('./lib/XLink_1_0'       ).XLink_1_0,
+			require('./lib/XSD_1_0'         ).XSD_1_0);
+	}
+}(this,
+function (exports, 
+	Atom_1_0,
+	WS_Addr_1_0_Core,
+	XHTML_1_0_Strict,
+	XLink_1_0,
+	XSD_1_0)
+{
+	exports.Atom_1_0         = Atom_1_0        ;
+	exports.WS_Addr_1_0_Core = WS_Addr_1_0_Core;
+	exports.XHTML_1_0_Strict = XHTML_1_0_Strict;
+	exports.XLink_1_0        = XLink_1_0       ;
+	exports.XSD_1_0          = XSD_1_0         ;
+}));
+
