4ab778a318ec3d39c47cd86ff90435ded135c5ea
André Vautour <andre.vautour@gmail.com>
2015-08-28 15:36:02 -0300

    Issue  #15. Enabled the generation of isSet methods to be consistent with the ogc-schemas project.

---
+++ XMLSchema/2001/src/test/java/org/hisrc/w3c/xmlschema/v_1_0/tests/IsSetMethodsGeneratedTest.java
@@ -0,0 +1,18 @@
+package org.hisrc.w3c.xmlschema.v_1_0.tests;
+
+import org.hisrc.w3c.xmlschema.v_1_0.Wildcard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IsSetMethodsGeneratedTest {
+
+    @Test
+    public void testIsSet() throws Exception {
+        Wildcard envelopeType = new Wildcard();
+        Assert.assertFalse(envelopeType.isSetNamespace());
+
+        envelopeType.getNamespace().add("http://www.opengis.net/ows/2.0");
+        Assert.assertTrue(envelopeType.isSetNamespace());
+    }
+}
